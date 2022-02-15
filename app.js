1c067a0c8b308404bab5f10602b30f41c1191826
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-02-15 22:23:04 +0200

    simple chart with random data

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-d3.select('.s')
-  .selectAll('p')
-  .data([1,2,3,4,5])
+
+const DUMMY_DATA = [
+  { id: 1, name: "alex", value: (Math.random() * 50).toFixed() },
+  { id: 2, name: "bob", value: (Math.random() * 50).toFixed() },
+  { id: 3, name: "carl", value: (Math.random() * 50).toFixed() },
+  { id: 4, name: "dave", value: (Math.random() * 50).toFixed() },
+  { id: 5, name: "ed", value: (Math.random() * 50).toFixed() },
+  { id: 6, name: "fred", value: (Math.random() * 50).toFixed() },
+  { id: 7, name: "george", value: (Math.random() * 50).toFixed() },
+  { id: 8, name: "harry", value: (Math.random() * 50).toFixed() },
+  { id: 9, name: "ian", value: (Math.random() * 50).toFixed() },
+  { id: 10, name: "joe", value: (Math.random() * 50).toFixed() },
+];
+const cont = d3
+  .select(".s")
+  .selectAll("p")
+  .data(DUMMY_DATA)
   .enter()
-  .append('p')
-  .text(data => data);
- 
+  .append("p")
+  .classed("bar", true)
+  .text((data) => data.name)
+  .style("width", value => `${value.value * 10}px`);
+
+cont.selectAll(".bar").data();
